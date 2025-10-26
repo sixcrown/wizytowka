5fd043d9fd2e2792d4712e05f479f72079fca9cf
sixcrown <kartoszbawalkiewicz@gmail.com>
2025-10-26 07:32:01 +0100

    req handler

--- api/index.mjs
+++ api/index.mjs
@@ -2,8 +2,8 @@
 
 export default async (req, res) => {
   try {
-    const { app } = await import('../dist/wizytowka-slub/server/server.mjs');
-    return app(req, res);
+    const { reqHandler } = await import('../dist/wizytowka-slub/server/server.mjs');
+    return reqHandler(req, res);
   } catch (error) {
     console.error('Error loading server module:', error);
     res.status(500).send('Server Error');
